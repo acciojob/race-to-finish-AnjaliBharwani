5adc7a81f07d015c48539956e0a16325f0acd4aa
AnjaliBharwani <139613568+AnjaliBharwani@users.noreply.github.com>
2023-12-07 21:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,17 @@
-// script.js
-const outputDiv = document.getElementById('output');
+  window.promises = [
+    new Promise(resolve => setTimeout(() => resolve('Promise 1 resolved'), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve('Promise 2 resolved'), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve('Promise 3 resolved'), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve('Promise 4 resolved'), getRandomTime())),
+    new Promise(resolve => setTimeout(() => resolve('Promise 5 resolved'), getRandomTime())),
+  ];
 
-// Function to generate a random time between 1 and 5 seconds
-function getRandomTime() {
-  return Math.floor(Math.random() * 5000) + 1000; // Between 1000 and 5000 milliseconds
-}
+  Promise.any(window.promises)
+    .then(result => {
+      document.getElementById('output').innerText = result;
+    })
+    .catch(error => console.log(error));
 
-// Array of promises with random resolving times
-const promises = Array.from({ length: 5 }, (_, index) =>
-  new Promise(resolve => {
-    const randomTime = getRandomTime();
-    setTimeout(() => {
-      resolve(`Promise ${index + 1} resolved in ${randomTime} milliseconds`);
-    }, randomTime);
-  })
-);
-
-// Use Promise.any to wait for the first resolved promise
-Promise.any(promises)
-  .then(result => {
-    outputDiv.textContent = result;
-  })
-  .catch(error => {
-    outputDiv.textContent = `All promises failed: ${error}`;
-  });
+  function getRandomTime() {
+    return Math.floor(Math.random() * (5000 - 1000 + 1) + 1000);
+  }
